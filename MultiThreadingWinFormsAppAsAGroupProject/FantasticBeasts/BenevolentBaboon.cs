34277327b9c5a2bf91819ffe9818faa328e1fa16
John Adkerson <adkersjv@mail.uc.edu>
2020-11-04 15:40:31 -0800

    Added necessary methods to unique beast class "BenevolentBaboon".

---
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/BenevolentBaboon.cs
@@ -0,0 +1,55 @@
+﻿/*****************************************************************
+ * John Adkerson
+ * Assignmnet06
+ * 11/04/2020
+ * adkersjv@mail.uc.edu
+ * Contemporary Programming IT3045C
+ * I chose my unique beast based off the google form sent out to the entire team
+ * request was factored seuccessfully
+ * ***************************************************************/
+using System;
+using System.Collections.Generic;
+
+using System.Threading;
+
+
+namespace MultiThreadingWinFormsAppAsAGroupProject
+{
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class BenevolentBaboon : FantasticBeast
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Hello from " + this.GetType());
+        }
+        public override void RunThread()
+        {
+            msg = "Hello from BenevolentBaboon.RunThread()";
+            Thread.Sleep(2000);
+            long num = Convert.ToInt64(request);
+            response = Convert.ToString(maxfactor(num));
+        }
+
+        static private long maxfactor(long request)
+        {
+            int potentialFactor = 2;
+            while (potentialFactor * potentialFactor <= request)
+            {
+                if (request % potentialFactor == 0)
+                {
+                    request /= potentialFactor;
+                }
+                else
+                {
+                    ++potentialFactor;
+                }
+            }
+
+            return request;
+        }
+    }
+
+}
+
